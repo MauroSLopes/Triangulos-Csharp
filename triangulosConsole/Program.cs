5554765e6b3ca3ae0ba61e77c01f45c58b4198ac
Mauro Santos Lopes <Mauro.slopes06@gmail.com>
2024-08-15 18:02:21 -0300

    Done

---
+++ triangulosConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿using triangulosConsole;
+
+int trianguloOpcao = 5;
+
+Triangulos tri = new Triangulos();
+
+Console.Clear();
+
+do
+{
+    Console.WriteLine("Digite a opção desejada:\n" +
+        "1 - Triangulo lateral de largura t e altura 2t-1.\n" +
+        "2 - Triangulo altura t e largura 2t-1.\n" +
+        "3 - Triangulo Retangulo.");
+    trianguloOpcao = Convert.ToInt32(Console.ReadLine());
+
+    Console.Clear();
+
+    switch (trianguloOpcao)
+    {
+        case 1:
+            tri.calcularEqui();
+            break;
+        case 2:
+            tri.calcularDiferente();
+            break;
+        case 3:
+            tri.calcularRet();
+            break;
+        default:
+            Console.WriteLine("Até breve");
+            trianguloOpcao = 0;
+            break;
+    }
+    Console.ReadLine();
+} while (trianguloOpcao != 0);
---
+++ triangulosConsole/Triangulos.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triangulosConsole
+{
+    public class Triangulos
+    {
+
+        public int lado { get; set; }
+
+        public Triangulos() 
+        {
+            Console.Write("Digite a altura do triangulo desejado: ");
+            this.lado = Convert.ToInt32(Console.ReadLine());
+        }
+        public void calcularEqui()
+        {
+            for (int i = 1; i <= lado; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            for (int i = 1; i <= lado; i++)
+            {
+                for (int j = lado; j > i; j--)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void calcularDiferente()
+        {
+            for (int i = 1; i <= lado; i++)
+            {
+                for (int j = 0; j < ((i*2)-1); j++)
+                { 
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        public void calcularRet()
+        {
+            for(int i = 1; i <= lado; i++)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
